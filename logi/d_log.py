e1a7d12bd210fcf243243038fcb0ad24a8cfae35
unknown <malolepszy.mikolaj00@gmail.com>
2025-12-08 22:15:09 +0100

    logi koniec

--- logi/d_log.py
+++ logi/d_log.py
@@ -1,33 +1,13 @@
 import logging
 import logging.config
+import json
 
+with open("cfg.json", "r") as f:
+    log_config = json.load(f)
 
-cfg = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "standardowy": {
-            "format": "%(asctiime)s :[%(levelname)s], %(message)s",
-            "datefmt": "%d-%m-%d -> %H:%M:%S",
-        },
-        "prościutki": {"format": "%(levelname)s: %(message)s"},
-    },
-    "handlers": {
-        "w_konsoli": {
-            "class": "logging.StreamHandler",
-            "level": "INFO",
-            "formatter": "prościutki",
-            "stream": "ext://sys.stdout",
-        },
-        "plik": {
-            "class": "logging.FileHandler",
-            "level": "ERROR",
-            "formatter": "standardowy",
-            "filename": "bledy_modelu.log",
-            "mode": "a",
-        },
-    },
-    "loggers": {
-        "": {"handlers": ["w_konsoli", "plik"], "level": "INFO", "propagate": True}
-    },
-}
+logging.config.dictConfig(log_config)
+logger = logging.getLogger(__name__)
+
+logger.info("Rozpoczęto wczytywanie danych treningowych")
+logger.warning("Nie znaleziono pliku konfiguracji, używam domyślnych parametrów.")
+logger.error("Błąd połączenia z bazą danych modeli!")
